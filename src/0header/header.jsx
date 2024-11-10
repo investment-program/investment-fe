70f5d750f22b08c2207b192f3f7f47fa3da363e7
dyoni2 <143973893+dyoni2@users.noreply.github.com>
2024-11-10 16:36:57 +0900

    Add files via upload

---
+++ src/0header/header.jsx
@@ -0,0 +1,23 @@
+import './header.css';
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+
+const activeStyle = {
+  color: 'black',
+};
+
+export default function Header() {
+  return (
+    <div className="header">
+      <header className="navbar">
+        <div className="nav-left">
+          <h3>Our Stock Project</h3>
+        </div>
+        <div className="nav-links">
+          <NavLink style={({ isActive }) => (isActive ? activeStyle : {})}  to="/" exact="true">조건설정</NavLink>
+          <NavLink style={({ isActive }) => (isActive ? activeStyle : {})}  to="/invest" exact="true">종목설정</NavLink>
+        </div>
+      </header>
+    </div>
+  );
+}
